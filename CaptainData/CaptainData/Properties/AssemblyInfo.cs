d3974ff626981e534b762a14fcff0f7ef4ea896d
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2016-04-04 15:47:03 +0200

    This is what I meant to do in my last commit
    need to add some tests to this code...

--- CaptainData/CaptainData/Properties/AssemblyInfo.cs
+++ CaptainData/CaptainData/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.12")]
-[assembly: AssemblyFileVersion("0.0.12")]
+[assembly: AssemblyVersion("0.0.13")]
+[assembly: AssemblyFileVersion("0.0.13")]
 [assembly: AssemblyProduct("Captain Data")]
 [assembly: AssemblyCompany("Mattias Nordqvist")]
 
