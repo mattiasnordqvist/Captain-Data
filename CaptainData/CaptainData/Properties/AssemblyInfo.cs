877e8aa809e19a5525ec7937b2b38b01e9467539
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2016-04-20 11:29:25 +0200

    upping version

--- CaptainData/CaptainData/Properties/AssemblyInfo.cs
+++ CaptainData/CaptainData/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.14")]
-[assembly: AssemblyFileVersion("0.0.14")]
+[assembly: AssemblyVersion("0.0.15")]
+[assembly: AssemblyFileVersion("0.0.15")]
 [assembly: AssemblyProduct("Captain Data")]
 [assembly: AssemblyCompany("Mattias Nordqvist")]
 
