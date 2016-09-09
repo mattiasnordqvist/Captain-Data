5ee0492b2c1e63abe2e154f0efe1781881569051
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2016-09-09 18:13:01 +0200

    forgot to fix dep in nuspec

--- CaptainData/CaptainData/Properties/AssemblyInfo.cs
+++ CaptainData/CaptainData/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.17")]
-[assembly: AssemblyFileVersion("0.0.17")]
+[assembly: AssemblyVersion("0.0.18")]
+[assembly: AssemblyFileVersion("0.0.18")]
 [assembly: AssemblyProduct("Captain Data")]
 [assembly: AssemblyCompany("Mattias Nordqvist")]
 
