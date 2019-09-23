16aa0fa993e74c95f596b6e86ee2dc0823d6fc93
Carl Berg <carl.berg@spinit.se>
2019-09-23 09:24:41 +0200

    Re-added NonEmptyColumnInstructions (not sure what they're for but i'll leave that up to @mattiasnordqvist)

--- CaptainData/CaptainData/RowInstruction.cs
+++ CaptainData/CaptainData/RowInstruction.cs
@@ -11,6 +11,14 @@ namespace CaptainData
 
         public object Overrides { get; set; }
 
+        internal IDictionary<string, ColumnInstruction> NonEmptyColumnInstructions
+        {
+            get
+            {
+                return ColumnInstructions.Where(x => x.Key != null).ToDictionary(x => x.Key, x => x.Value);
+            }
+        }
+
         public Dictionary<string, ColumnInstruction> ColumnInstructions { get; private set; } = new Dictionary<string, ColumnInstruction>();
 
         public ColumnInstruction this[string index]
